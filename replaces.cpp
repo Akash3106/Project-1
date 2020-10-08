2bfa6e85029c49a19f11cf8f41657babe6e6a135
Akash3106 <58002854+Akash3106@users.noreply.github.com>
2020-10-08 22:48:11 +0530

    Create replaces.cpp
    new code

---
+++ replaces.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h> 
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int t;
+    long n,p,k;
+    long long x,el;
+    
+    //a1
+
+
+
+        sort(arr.begin(),arr.end());
+        //a2
+
+
+        else{
+
+            //a3
+            
+                else
+                if((x<arr[p-1] && p<=k )|| (x>arr[p-1] && p>=k)){
+                    long long c=count(arr.begin(), arr.end(), x);
+                    it =find (arr.begin(), arr.end(), x);
+                    if(c==1){
+                        // cout<<":6";
+                        count1=abs(it-arr.begin()-(p-1))+1;
+                    }
+                    else{
+                        if(kval>x){
+                           if(it-arr.begin()<p){
+                            // cout<<":7";
+                            count1=abs(abs(it-arr.begin()-(p-1))-(c-1))+1;
+                        }
+                        else{
+                            // cout<<":8";
+                            count1=abs(it-arr.begin()-(p-1));
+                        } 
+                        }
+                        else
+                        if(kval<x){
+                            if(it-arr.begin()<p){
+                            // cout<<":7";
+                            count1=abs(abs(it-arr.begin()-(p-1))-(c-1))-1;
+                        }
+                        else{
+                            // cout<<":8";
+                            count1=abs(it-arr.begin()-(p-1))+1;
+                        }
+                        }
+                        else{
+                            if(it-arr.begin()<p){
+                            // cout<<":7";
+                            count1=abs(abs(it-arr.begin()-(p-1))-(c-1));
+                        }
+                        else{
+                            // cout<<":8";
+                            count1=abs(it-arr.begin()-(p-1));
+                        }
+                        }
+                        
+                        
+                    }
+                   //a4
+
+            }
+       
+    }
+    }
+    return 0;
+}
